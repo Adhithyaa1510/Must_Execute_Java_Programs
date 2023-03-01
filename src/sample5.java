4a6c088b4253a6e56d91624939810ed37d11c7bf
Adhithyaa1510 <aathimusic@gmail.com>
2023-03-01 14:01:31 +0530

    commit on 01.03.2023

--- src/sample5.java
+++ src/sample5.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class sample5 {
     public static void main(String args[])
-    {
-        
+    {  int i=0;
+       char a[]=new char[30];
+       char b[]=new char[30];
+       Scanner sc=new Scanner(System.in);
+       String name=sc.nextLine();
+       for(i=0;i!=(name.length());i++)
+       {
+         a[i]=name.charAt(i);
+       }
+       while(b[i++]==a[i++]);
     }
-    
 }
